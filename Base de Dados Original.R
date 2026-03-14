588ba18f7c682c361fb628958b4a6413241f4f38
Lucas Camaz Ferreira <camazlucas@gmail.com>
2026-03-14 17:47:44 -0300

    Separação da Filtragem dos Dados pros Dados por Estado e Adição da Base de Dados Originais que geram as classes sociais

---
+++ Base de Dados Original.R
@@ -0,0 +1,39 @@
+# Selecionando dados padroes para a analise de classes --------------------
+dados_originais = function(){
+  dados_orig = dados[, c(7:22, 51)]
+  
+  summary(dados_orig)
+  
+  #Alterando dados do tipo character para dados numericos
+  dados_orig$P3.1_3 = as.numeric(gsub(",", "", dados_orig$P3.1_3))
+  dados_orig$P3.1_4 = as.numeric(gsub(",", "", dados_orig$P3.1_4))
+  dados_orig$P3.1_5 = as.numeric(gsub(",", "", dados_orig$P3.1_5))
+  dados_orig$P3.1_12 = as.numeric(gsub(",", "", dados_orig$P3.1_12))
+  dados_orig[is.na(dados_orig)] = 0
+  
+  
+  dados_orig = subset(dados_orig, P5.13 == 1)
+  
+  names(dados_orig) = c("Automoveis", 
+                        "Empregados", 
+                        "Lava_Roupas", 
+                        "Banheiros", 
+                        "DVD", 
+                        "Geladeiras", 
+                        "Freezers", 
+                        "Microcomputadores", 
+                        "Lava_Loucas", 
+                        "Microondas", 
+                        "Motocicletas", 
+                        "Seca_Roupas",
+                        "Fonte_da_Agua",
+                        "Rua",
+                        "Instrucao",
+                        "CLASSE")
+  
+  dados_orig = dados_orig[,-17]
+  
+  return(dados_orig)
+}
+
+dados_o = dados_originais()
